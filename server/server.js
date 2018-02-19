db7c5de0f6cdfa9c90b27c0cdc013feab43eba88
Roman <rm07ru@gmail.com>
2018-02-19 18:54:42 +0300

    Connect to mongo db

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
+const secrets = require('./secrets');
 
 const app = express();
 
-// Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = `mongodb://${secrets.DB_USER}:${secrets.DB_PW}@ds243008.mlab.com:43008/lyricaldb`;
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
