48a339dbbe1ed3b792f86c0a73810b78774e435c
Roman <rm07ru@gmail.com>
2018-03-09 12:43:46 +0300

    Add like button

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class LyricList extends Component {
+  onLike(id) {
+    console.log(id);
+  }
+  
   render() {
     return (
       <ul className="collection">
@@ -9,6 +13,14 @@ class LyricList extends Component {
             return (
               <li key={id} className="collection-item">
                 {content}
+                <span className="secondary-content">
+                  <i
+                    className="material-icons"
+                    onClick={() => this.onLike(id)}
+                  >
+                    thumb_up
+                  </i>
+                </span>
               </li>
             )
           })
